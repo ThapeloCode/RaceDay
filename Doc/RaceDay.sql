6b28cdecb5271a282513340effe7d42bc897aa8a
ThapeloCode <thplhpp@gmail.com>
2026-09-04 17:13:47 +0200

    Added Category Table

--- Doc/RaceDay.sql
+++ Doc/RaceDay.sql
@@ -1,5 +1,8 @@
 USE RaceDayDb;
 
-INSERT INTO dbo.Event (Title, Description, Start_Date, End_Date, Location) VALUES
-('Midmar Mile Open Water Swim', 'The worlds largest open water swimming event at Midmar Dam.', '2026-11-14 07:00:00', '2026-11-15 15:00:00', 'Midmar Dam, Howick, KZN'),
-('Cape Town Cycle Tour', 'Iconic cycling route around the Cape Peninsula.', '2026-12-06 06:00:00', '2026-12-06 17:00:00', 'Grand Parade, Cape Town');
+INSERT INTO dbo.Category (Event_Id, Title) VALUES
+(1, '10K Express Run'),
+(1, '21K Half Marathon'),
+(1, '42K Ultra/Full Marathon'),
+(2, '1.6K Mile Open Swim'),
+(3, '109K Classic Peninsula');
