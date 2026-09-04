0070b5a68c95e5f830edad7ab8a8aa02f483fc08
ThapeloCode <thplhpp@gmail.com>
2026-09-04 18:08:12 +0200

    Improved the readability of time to Hour|Minutes|Seconds

--- Doc/RaceDay.sql
+++ Doc/RaceDay.sql
@@ -1,10 +1,20 @@
 USE RaceDayDb;
 GO
 
-USE RaceDayDb;
-GO
-
-INSERT INTO dbo.Results (Registration_Id, Finish_Time_Seconds) VALUES
-(5, 5940), -- 1 hr 39
-(6, 6840), -- 1 hr 54 
-(8, 2100); -- 35 mins
+SELECT 
+    r.Id AS RegistrationId,
+    CONCAT(u.First_Name, ' ', u.Last_Name) AS ParticipantName,
+    e.Title AS EventTitle,
+    c.Title AS CategoryTitle,
+    r.Bib_Number,
+    ISNULL(
+        RIGHT('0' + CAST(res.Finish_Time_Seconds / 3600 AS VARCHAR), 2) + ':' +
+        RIGHT('0' + CAST((res.Finish_Time_Seconds % 3600) / 60 AS VARCHAR), 2) + ':' +
+        RIGHT('0' + CAST(res.Finish_Time_Seconds % 60 AS VARCHAR), 2),
+        'Pending'
+    ) AS FormattedFinishTime
+FROM dbo.Registration r
+JOIN dbo.[User] u ON r.User_Id = u.Id
+JOIN dbo.Category c ON r.Category_Id = c.Id
+JOIN dbo.Event e ON c.Event_Id = e.Id
+LEFT JOIN dbo.Results res ON res.Registration_Id = r.Id;
