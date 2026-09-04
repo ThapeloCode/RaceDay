f2108035bf9df784477e27e5478e6270369b1244
ThapeloCode <thplhpp@gmail.com>
2026-09-04 10:44:23 +0200

    Added Events Added

--- Doc/RaceDay.sql
+++ Doc/RaceDay.sql
@@ -1,9 +1,18 @@
 --CREATE DATABASE RaceDayDb;
-CREATE TABLE dbo.[User] (
+/*CREATE TABLE dbo.[User] (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     First_Name NVARCHAR(50) NOT NULL,
     Last_Name NVARCHAR(50) NOT NULL,
     Role NVARCHAR(20) NOT NULL CONSTRAINT CK_User_Role CHECK (Role IN ('Admin', 'Participant')),
     email NVARCHAR(100) NOT NULL UNIQUE,
     Password_Hash NVARCHAR(255) NOT NULL
+);*/
+CREATE TABLE dbo.Event (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Title NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(MAX) NULL,
+    Start_Date DATETIME2 NOT NULL,
+    End_Date DATETIME2 NOT NULL,
+    Location NVARCHAR(150) NOT NULL,
+    CONSTRAINT CK_Event_Dates CHECK (End_Date >= Start_Date)
 );
