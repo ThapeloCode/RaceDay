842aeabc2e6762de918a2d03aa63dacee7453823
ThapeloCode <thplhpp@gmail.com>
2026-09-04 10:41:20 +0200

    Added User

---
+++ Doc/RaceDay.sql
@@ -0,0 +1,9 @@
+--CREATE DATABASE RaceDayDb;
+CREATE TABLE dbo.[User] (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    First_Name NVARCHAR(50) NOT NULL,
+    Last_Name NVARCHAR(50) NOT NULL,
+    Role NVARCHAR(20) NOT NULL CONSTRAINT CK_User_Role CHECK (Role IN ('Admin', 'Participant')),
+    email NVARCHAR(100) NOT NULL UNIQUE,
+    Password_Hash NVARCHAR(255) NOT NULL
+);
