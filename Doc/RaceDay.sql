a0893ca6cce7b7c38cf0d41dc23034a38dad1c6d
ThapeloCode <thplhpp@gmail.com>
2026-09-04 16:30:50 +0200

    Populated Table with 3 Events

--- Doc/RaceDay.sql
+++ Doc/RaceDay.sql
@@ -1,4 +1,5 @@
 USE RaceDayDb;
 
-INSERT INTO dbo.[User] (First_Name, Last_Name, Role, email, Password_Hash) VALUES
-('Keegan', 'Naidoo', 'Participant', 'keegan.n@outlook.com', 'AQAAAAEAACcQAAAAE44A1...');
+INSERT INTO dbo.Event (Title, Description, Start_Date, End_Date, Location) VALUES
+('Midmar Mile Open Water Swim', 'The worlds largest open water swimming event at Midmar Dam.', '2026-11-14 07:00:00', '2026-11-15 15:00:00', 'Midmar Dam, Howick, KZN'),
+('Cape Town Cycle Tour', 'Iconic cycling route around the Cape Peninsula.', '2026-12-06 06:00:00', '2026-12-06 17:00:00', 'Grand Parade, Cape Town');
