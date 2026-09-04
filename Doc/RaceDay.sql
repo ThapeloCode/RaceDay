e36ff77308fd5b10eeba02937771f77033d23162
ThapeloCode <thplhpp@gmail.com>
2026-09-04 17:33:00 +0200

    Populated Route Table

--- Doc/RaceDay.sql
+++ Doc/RaceDay.sql
@@ -1,8 +1,3 @@
 USE RaceDayDb;
 
-INSERT INTO dbo.Category (Event_Id, Title) VALUES
-(1, '10K Express Run'),
-(1, '21K Half Marathon'),
-(1, '42K Ultra/Full Marathon'),
-(2, '1.6K Mile Open Swim'),
-(3, '109K Classic Peninsula');
+SELECT * FROM dbo.Route;
