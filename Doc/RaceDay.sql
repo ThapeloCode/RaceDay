dc2e3d3a3c8087103dfc9a6550b4d6941064e4bb
ThapeloCode <thplhpp@gmail.com>
2026-09-04 15:47:53 +0200

    Registration Table Added

--- Doc/RaceDay.sql
+++ Doc/RaceDay.sql
@@ -1,12 +1,13 @@
 --CREATE DATABASE RaceDayDb;
-/*CREATE TABLE dbo.[User] (
+CREATE TABLE dbo.[User] (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     First_Name NVARCHAR(50) NOT NULL,
     Last_Name NVARCHAR(50) NOT NULL,
     Role NVARCHAR(20) NOT NULL CONSTRAINT CK_User_Role CHECK (Role IN ('Admin', 'Participant')),
     email NVARCHAR(100) NOT NULL UNIQUE,
     Password_Hash NVARCHAR(255) NOT NULL
-);*/
+);
+
 CREATE TABLE dbo.Event (
     Id INT IDENTITY(1,1) PRIMARY KEY,
     Title NVARCHAR(100) NOT NULL,
@@ -31,4 +32,16 @@ CREATE TABLE dbo.Route (
     Location NVARCHAR(150) NOT NULL,
     Distance_km DECIMAL(5,2) NOT NULL CONSTRAINT CK_Route_Distance CHECK (Distance_km > 0),
     CONSTRAINT FK_Route_Category FOREIGN KEY (Category_Id) REFERENCES dbo.Category(Id) ON DELETE CASCADE
+);
+
+CREATE TABLE dbo.Registration (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    User_Id INT NOT NULL,
+    Category_Id INT NOT NULL,
+    Bib_Number INT NOT NULL,
+    Date DATETIME2 NOT NULL DEFAULT GETDATE(),
+    CONSTRAINT FK_Registration_User FOREIGN KEY (User_Id) REFERENCES dbo.[User](Id),
+    CONSTRAINT FK_Registration_Category FOREIGN KEY (Category_Id) REFERENCES dbo.Category(Id),
+    CONSTRAINT UQ_User_Category UNIQUE (User_Id, Category_Id), -- Prevents duplicate registrations
+    CONSTRAINT UQ_Category_Bib UNIQUE (Category_Id, Bib_Number) -- Unique Bib Number per Category
 );
